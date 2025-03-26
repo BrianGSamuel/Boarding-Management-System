3b5952e35a38240b305544521aa993314152b755
Brian.G.Samuel <167592418+BrianGSamuel@users.noreply.github.com>
2025-03-26 21:27:38 +0530

    commit

--- Backend/models/Serviceprovider.js
+++ Backend/models/Serviceprovider.js
@@ -1,29 +1,43 @@
 const mongoose = require('mongoose');
-
 const Schema = mongoose.Schema;
 
 // Define the schema for ServiceProvider
 const ServiceProviderSchema = new Schema({
-    userId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true,
-    },
-    name: { type: String, required: true },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        match: [/\S+@\S+\.\S+/, 'Please use a valid email address'],
-    },
-    phoneNumber: { type: String, required: true },
-    serviceArea: { type: String, required: true }, // Location or area where service is provided
-    serviceType: { type: String, required: true }, // Type of service offered
-    description: { type: String, required: false }, // Additional details about the service
-    createdAt: {
-        type: Date,
-        default: Date.now,
-    },
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/\S+@\S+\.\S+/, 'Please use a valid email address']
+  },
+  phoneNumber: {
+    type: String,
+    required: true
+  },
+  serviceArea: {
+    type: String,
+    required: true
+  }, // Location or area where service is provided
+  serviceType: {
+    type: String,
+    required: true
+  }, // Type of service offered
+  description: {
+    type: String,
+    required: false
+  }, // Additional details about the service
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  status: {
+    type: String,
+    enum: ['unverified', 'verified'],
+    default: 'unverified' // New field to track the verification status
+  }
 });
 
 // Create the model
